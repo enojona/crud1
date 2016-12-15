e5d97ed7c6651080b33db8bcc0dfb713e467a36c
enojona <karea.221997@gmail.com>
2016-12-15 18:47:45 -0500

    holaprueba

--- inc/conexion.php
+++ inc/conexion.php
@@ -3,7 +3,7 @@
 	</head>
 	<body>
 		<?
-			$conexion = new mysqli("localhost","root","root",'programacionweb');
+			$conexion = new mysqli("mysql.hostinger.es","u222523177_karen","karen2016xx",'u222523177_karen');
 			if($conexion->conect_errno){
 				echo "Error al connectar a la BBDD".
 					$conexion->connect_errno.",".
